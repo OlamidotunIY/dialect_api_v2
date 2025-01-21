98dded7445890b31e9f7d625c196ed13c7e80209
Iyanda Olamidotun <olamidotun225@gmail.com>
2025-01-21 10:58:55 +0100

    fix: access issues

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,15 @@ import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
 
+const allowedOrigins = [
+  process.env.FRONTEND_URL, // Your first frontend URL
+  'http://api.dialects.uno:3500/',      // Any additional URL
+];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: allowedOrigins,
     credentials: true,
 
     allowedHeaders: [
