c3966f619a8e0398ceac5fbd8dc72caf86623115
Iyanda Olamidotun <olamidotun225@gmail.com>
2025-02-10 17:55:05 +0100

    add: teams

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TeamService } from './team.service';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.services';
 
-@Module({})
-export class TeamModule {}
+@Module({
+    providers: [PrismaService, JwtService, TeamService],
+})
+export class TeamModule {
+
+}
